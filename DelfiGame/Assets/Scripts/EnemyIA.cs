b34d4098e5e1949d43965b0dcf7365d843e9b54d
Delfi <martinfreirep@gmail.com>
2024-04-30 00:26:14 +0200

    IA de enemigos
    Estoy intentando mejorar la IA de los enemigos, cree un enum para los estados, ahora al empezar esta roaming cogiendo puntos aleatorios, si el personaje entra en rango lo persigue y si se acerca más le empieza a disparar, no me convence aún, tengo que tener en cuenta si se choca con paredes y que no vea atravesando paredes al jugador, pero no sé como hacer eso aún, tengo pensado que vuelva a su posición inicial si se aleja mucho el jugador pero eso está comentado que no funcionaba bien. Por hoy lo deho que ya son las 12:30 y tengo que madrugar

--- DelfiGame/Assets/Scripts/EnemyIA.cs
+++ DelfiGame/Assets/Scripts/EnemyIA.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyIA : MonoBehaviour
@@ -7,16 +8,56 @@ public class EnemyIA : MonoBehaviour
     public GameObject player;
     public float moveSpeed = 3f;
     public float rotationSpeed = 3f;
+    private Vector3 startingPosition; //para el comportamiento de Roam 
+    private Vector3 roamingPosition;
 
     private EnemyAttack enemyAttack;
+
+    private enum State
+    {
+        Roaming,
+        ChaseTarget,
+        GoingBackToStart,
+    }
+    private State state;
+    private void Awake()
+    {
+        state = State.Roaming;
+    }
     void Start()
     {
         enemyAttack = this.GetComponent<EnemyAttack>();
+        startingPosition = this.transform.position;
+        roamingPosition = GetRoamingPosition();
     }
 
     void Update()
     {
-        FindPlayer();
+        switch (state)
+        {
+            case State.Roaming:
+                MoveTo(roamingPosition);
+                if (Vector3.Distance(transform.position, roamingPosition) < 0.5f) //0.2 para dejar margen
+                {
+                    roamingPosition = GetRoamingPosition();
+                }
+                FindPlayer();
+
+                break;
+            case State.ChaseTarget:
+                ChasePlayer();
+                break;
+            case State.GoingBackToStart:
+                //MoveTo(startingPosition);
+                //if (Vector3.Distance(transform.position, startingPosition) < 0.5f)
+                //{
+      
+                //    state = State.Roaming;
+                //}
+                break;
+        }
+
+
     }
 
     private void FindPlayer()
@@ -26,24 +67,71 @@ public class EnemyIA : MonoBehaviour
         //TODO HACER CONDICION QUE SI EL JUGADOR DISPARA LOS ENEMIGOS SE ALERTEN!!!
         if (Vector3.Distance(transform.position, player.transform.position) < playerRange)
         {
-
-            Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
-
-            // rotar al sprite para que nos mire
-            Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, directionToPlayer);
-            targetRotation *= Quaternion.Euler(0, 0, 90);
-
-
-            //  la rotación al enemigo
-            transform.rotation = Quaternion.Euler(0f, 0f, targetRotation.eulerAngles.z);
-
-            // mover al enemigo
-            transform.Translate(directionToPlayer * moveSpeed * Time.deltaTime, Space.World);
-
-
-
-            enemyAttack.testShooting();
-
+            state = State.ChaseTarget;
         }
     }
+
+    private void ChasePlayer()
+    {
+
+        Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
+
+        // rotar al sprite para que nos mire
+        Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, directionToPlayer);
+        targetRotation *= Quaternion.Euler(0, 0, 90);
+
+
+        //  la rotación al enemigo
+        transform.rotation = Quaternion.Euler(0f, 0f, targetRotation.eulerAngles.z);
+
+        // mover al enemigo
+        transform.Translate(directionToPlayer * moveSpeed * Time.deltaTime, Space.World);
+
+
+
+        float attackRange = 3f;
+        if (Vector3.Distance(transform.position, player.transform.position) < attackRange)
+        {
+            enemyAttack.testShooting();
+        }
+
+
+        //float stopChaseRange = 15f;
+        //if (Vector3.Distance(transform.position, player.transform.position) < stopChaseRange)
+        //{
+        //    state = State.GoingBackToStart;
+        //}
+    }
+
+    public void MoveTo(Vector3 targetPosition)
+    {
+        SetTargetPosition(targetPosition);
+    }
+
+    public void SetTargetPosition(Vector3 targetPosition)
+    {
+
+        Vector3 directionToTarget = (targetPosition - transform.position).normalized;
+
+        Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, directionToTarget);
+        targetRotation *= Quaternion.Euler(0, 0, 90);
+        transform.rotation = Quaternion.Euler(0f, 0f, targetRotation.eulerAngles.z);
+
+        transform.Translate(directionToTarget * moveSpeed * Time.deltaTime, Space.World);
+
+    }
+
+    private Vector3 GetRoamingPosition()
+    {
+        return startingPosition + GetRandomDir() * Random.Range(5f, 10f);
+    }
+
+    public Vector3 GetRandomDir()
+    {
+        return new Vector3(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f)).normalized;
+    }
 }
+
+//HACER FUNCION PARA MOVER Y ROTAR
+
+//CUANDO SE CHOQUE QUE GIRE
